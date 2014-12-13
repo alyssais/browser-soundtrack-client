d9af5ea573f2782fde7d5eb7fb4f9541a667ebfa
Alyssa Ross <hi@alyssa.is>
2014-12-13 13:06:35 +0000

    Log URL changes

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log(tabId, changeInfo, tab);
+  if (changeInfo.hasOwnProperty("url")) {
+    console.log("tab #" + tabId + " changed URL to " + changeInfo.url);
+  }
 });
