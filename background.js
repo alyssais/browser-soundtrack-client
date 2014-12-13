6fb083f8b72838873686bc2b541b343adda9d19d
Alyssa Ross <hi@alyssa.is>
2014-12-13 22:20:09 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.hasOwnProperty("url")) {
     console.log("tab #" + tabId + " changed URL to " + changeInfo.url);
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://cryptic-anchorage-3829.herokuapp.com/" + changeInfo.url, true);
+    request.onload = function() {
+      console.log(this.response);
+    };
   }
 });
