4245871fb79ad63e7b17f78d475b731d2e04a2c9
Alyssa Ross <hi@alyssa.is>
2014-12-14 11:13:28 +0000

    Update background.js

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ document.body.appendChild(audio);
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.hasOwnProperty("url")) {
+    chrome.notifications.clear(audio.src);
     audio.pause();
     var request = new XMLHttpRequest();
     request.open("GET", "http://cryptic-anchorage-3829.herokuapp.com/" + changeInfo.url, true);
@@ -11,7 +12,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         var song = JSON.parse(this.response);
         audio.src = song.audio_url;
         audio.play();
-        chrome.notifications.create(Math.random().toString(), {
+        chrome.notifications.create(audio.src, {
           type: "basic",
           title: song.title,
           message: song.artist,
