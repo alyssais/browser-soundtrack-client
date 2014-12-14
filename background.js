1d775505cd817b06548b99a2aeadb2d754986b75
Alyssa Ross <hi@alyssa.is>
2014-12-14 11:24:37 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,25 +1,35 @@
 var audio = document.createElement("audio");
 document.body.appendChild(audio);
 
+var urlChanged = function(url) {
+  chrome.notifications.clear(audio.src, function() {});
+  audio.pause();
+  var request = new XMLHttpRequest();
+  request.open("GET", "http://cryptic-anchorage-3829.herokuapp.com/" + url, true);
+  request.onload = function() {
+    if (this.status == 200) {
+      var song = JSON.parse(this.response);
+      audio.src = song.audio_url;
+      audio.play();
+      chrome.notifications.create(audio.src, {
+        type: "basic",
+        title: song.title,
+        message: song.artist,
+        iconUrl: song.artwork_url
+      }, function() {});
+    }
+  };
+  request.send();
+};
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.hasOwnProperty("url")) {
-    chrome.notifications.clear(audio.src, function() {});
-    audio.pause();
-    var request = new XMLHttpRequest();
-    request.open("GET", "http://cryptic-anchorage-3829.herokuapp.com/" + changeInfo.url, true);
-    request.onload = function() {
-      if (this.status == 200) {
-        var song = JSON.parse(this.response);
-        audio.src = song.audio_url;
-        audio.play();
-        chrome.notifications.create(audio.src, {
-          type: "basic",
-          title: song.title,
-          message: song.artist,
-          iconUrl: song.artwork_url
-        }, function() {});
-      }
-    };
-    request.send();
+    urlChanged(changeInfo.url);
   }
+});
+
+chrome.tabs.onActivated.addListener(function(tabInfo) {
+  chrome.tabs.get(tabInfo.tabId, function(tab) {
+    urlChanged(tab.url);  
+  });
 });
